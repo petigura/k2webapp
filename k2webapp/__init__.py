2ed8e9ba49b25a7e6685832473372c60ee709a87
Erik Petigura <epetigura@berkeley.edu>
2015-06-26 13:52:48 -0700

    Updated bootstrap CDN from netdna to maxcdn

--- k2webapp/__init__.py
+++ k2webapp/__init__.py
@@ -11,7 +11,9 @@ app.config['port'] = int(os.environ['K2WEBAPP_PORT'])
 
 @app.route('/vetting/<k2_camp>/<run>/<starname_url>',methods=['GET','POST'])
 def display_vetting(k2_camp,run,starname_url):
+    print k2webapp.views.display_vetting(k2_camp,run,starname_url)
     return k2webapp.views.display_vetting(k2_camp,run,starname_url)
+    
 
 @app.route('/vetting/list/<k2_camp>/<run>/',methods=['GET','POST'])
 def display_vetting_list(k2_camp,run):
