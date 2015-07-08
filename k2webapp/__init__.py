aef07f52e17643530e615d6c9fa713e27acb7dad
Erik Petigura <epetigura@berkeley.edu>
2015-07-08 06:55:28 -0700

    starname_url -> starname

--- k2webapp/__init__.py
+++ k2webapp/__init__.py
@@ -9,16 +9,13 @@ app.secret_key = os.urandom(24)
 app.config['host'] = os.environ['K2WEBAPP_HOST']
 app.config['port'] = int(os.environ['K2WEBAPP_PORT'])
 
-@app.route('/photometry/<k2_camp>/<run>/<starname_url>')
-def display_photometry(k2_camp,run,starname_url):
-    print "hello"
+@app.route('/photometry/<k2_camp>/<run>/<starname>')
+def display_photometry(k2_camp,run,starname):
+    return k2webapp.views.display_photometry( k2_camp,run,starname)
 
-    print k2webapp.views.display_photometry( k2_camp,run,starname_url)
-    return k2webapp.views.display_photometry( k2_camp,run,starname_url)
-
-@app.route('/vetting/<k2_camp>/<run>/<starname_url>',methods=['GET','POST'])
-def display_vetting(k2_camp,run,starname_url):
-    return k2webapp.views.display_vetting(k2_camp,run,starname_url)
+@app.route('/vetting/<k2_camp>/<run>/<starname>',methods=['GET','POST'])
+def display_vetting(k2_camp,run,starname):
+    return k2webapp.views.display_vetting(k2_camp,run,starnamey)
     
 
 @app.route('/vetting/list/<k2_camp>/<run>/',methods=['GET','POST'])
